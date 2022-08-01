4c58f8d8bced565c78fed6b15069a1ed7b77b383
Akzholtay Dagzhan <akzholtay.dagzhan@dataart.com>
2022-08-01 16:11:07 +0600

    fix: input value

--- example.js
+++ example.js
@@ -3,7 +3,7 @@ import {num2wordKZ} from "./src/num2wordKZ.js";
 const input = document.getElementById('input');
 const strInput = document.getElementById('text');
 input.addEventListener('change', function(event) {
-    console.log(input.target.value);
-    const str = input.target.value;
+    console.log(event.target.value);
+    const str = event.target.value;
     strInput.innerText = num2wordKZ(str);
 });
