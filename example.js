337ebaa0f03883303023bcb6acd831a3e3749b5f
Akzholtay Dagzhan <akzholtay.dagzhan@dataart.com>
2022-08-01 17:52:03 +0600

    1.0.23

--- example.js
+++ example.js
@@ -1,9 +1,9 @@
-import num2wordKZ from "./src/num2word.js";
+import num2word from "./src/num2word.js";
 
 const input = document.getElementById('input');
 const strInput = document.getElementById('text');
 input.addEventListener('change', function(event) {
     console.log(event.target.value);
     const str = event.target.value;
-    strInput.innerText = num2wordKZ(str);
+    strInput.innerText = num2word(str);
 });
