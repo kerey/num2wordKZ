7751275a290aeb84c6604c3b1b2363806eaef042
Akzholtay Dagzhan <akzholtay.dagzhan@dataart.com>
2022-08-01 18:55:23 +0600

    1.0.25

--- example.js
+++ example.js
@@ -2,7 +2,7 @@ import num2word from "./src/num2word.js";
 
 const input = document.getElementById('input');
 const strInput = document.getElementById('text');
-input.addEventListener('change', function(event) {
+input.addEventListener('keydown', function(event) {
     console.log(event.target.value);
     const str = event.target.value;
     strInput.innerText = num2word(str);
