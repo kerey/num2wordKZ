d362e8a50db170ff77f1ff026bea3f4dd226e4e0
Akzholtay Dagzhan <akzholtay.dagzhan@dataart.com>
2022-08-02 12:32:34 +0600

    fix: keypress

--- example.js
+++ example.js
@@ -7,7 +7,7 @@ const strInput = document.getElementById('text');
 const ATAU = document.getElementById('atau');
 const ILIK = document.getElementById('ilik');
 
-input.addEventListener('keyup', function(event) {
+input.addEventListener('keypress', function(event) {
     const str = event.target.value;
     const word = num2word(str);
     strInput.innerText = word;
